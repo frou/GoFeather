add9d30515aac15c16b456f3333184bddfc002a1
Duncan Holm <duncan.holm@moray.gov.uk>
2017-06-06 10:07:28 +0100

    Update keybindings for stub extract var plugin

--- plugin_extract_variable.py
+++ plugin_extract_variable.py
@@ -1,3 +1,9 @@
-# TODO(DH): Similar to extract function, use godoctor's extract variable mode.
-# Bind this to alt+v
+import sublime, sublime_plugin
+import subprocess, sys
 
+# TODO(DH): Similar to extract function, use godoctor's extract variable mode.
+class ExtractSelectionAsVariable(sublime_plugin.TextCommand):
+    def run(self, args):
+        view = self.view
+        window = view.window()
+        print("V")
