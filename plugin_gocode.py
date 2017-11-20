bc12c7eba1515e2a72b184bc89020cc64f50434e
Duncan Holm <duncan.holm@moray.gov.uk>
2017-11-20 13:20:04 +0000

    gocode: Strip out gofmt stuff

--- plugin_gocode.py
+++ plugin_gocode.py
@@ -92,38 +92,6 @@ def hint_and_subj(cls, name, type):
         hint = cls + " " + name + "\t" + type
     return hint, subj
 
-def diff_sanity_check(a, b):
-    if a != b:
-        raise Exception("diff sanity check mismatch\n-%s\n+%s" % (a, b))
-
-class GocodeGofmtCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        view = self.view
-        src = view.substr(sublime.Region(0, view.size()))
-        gofmt = subprocess.Popen(["gofmt"],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        sout, serr = gofmt.communicate(src.encode())
-        if gofmt.returncode != 0:
-            print(serr.decode(), end="")
-            return
-
-        newsrc = sout.decode()
-        diff = difflib.ndiff(src.splitlines(), newsrc.splitlines())
-        i = 0
-        for line in diff:
-            if line.startswith("?"): # skip hint lines
-                continue
-
-            l = (len(line)-2)+1
-            if line.startswith("-"):
-                diff_sanity_check(view.substr(sublime.Region(i, i+l-1)), line[2:])
-                view.erase(edit, sublime.Region(i, i+l))
-            elif line.startswith("+"):
-                view.insert(edit, i, line[2:]+"\n")
-                i += l
-            else:
-                diff_sanity_check(view.substr(sublime.Region(i, i+l-1)), line[2:])
-                i += l
 
 class Gocode(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
@@ -145,8 +113,3 @@ class Gocode(sublime_plugin.EventListener):
             result.append([hint, subj])
 
         return (result, sublime.INHIBIT_WORD_COMPLETIONS)
-
-    def on_pre_save(self, view):
-        if not view.match_selector(0, "source.go"):
-            return
-        view.run_command('gocode_gofmt')
