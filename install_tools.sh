2b0d78079d105f010adbe55d71acd9a1aa017b95
Duncan Holm <duncan@holm.scot>
2017-12-07 12:37:33 +0000

    Use `gometalinter` exclusively for Analyze variant

--- install_tools.sh
+++ install_tools.sh
@@ -11,9 +11,18 @@ upstall() {
 }
 
 upstall github.com/godoctor/godoctor
-upstall github.com/golang/lint/golint
 upstall github.com/nsf/gocode
 upstall golang.org/x/tools/cmd/goimports
 upstall golang.org/x/tools/cmd/gorename
 upstall golang.org/x/tools/cmd/guru
-upstall honnef.co/go/tools/cmd/megacheck
+
+# gometalinter does something complex where it vendors the source code of all
+# the linters it knows about, and then itself provides a command-line flag to
+# take resposibility for building and installing them.
+#
+# The following should provide a smooth install/update of gometalinter and its
+# linters:
+GML="github.com/alecthomas/gometalinter"
+rm -rf "$GOPATH/src/$GML"
+upstall "$GML"
+gometalinter --install --update
