1e3ff6fb6df6bb3830625bc582cb3a9022510bc9
Duncan Holm <duncan@holm.scot>
2018-03-07 11:03:44 +0000

    Rewrite comment; Add TODO

--- install_tools.sh
+++ install_tools.sh
@@ -17,12 +17,12 @@ upstall golang.org/x/tools/cmd/gorename
 upstall golang.org/x/tools/cmd/guru
 
 # gometalinter does something complex where it vendors the source code of all
-# the linters it knows about, and then itself provides a command-line flag to
+# the linter commands it knows about, and itself provides a runtime flag to
 # take resposibility for building and installing them.
 #
-# The following should provide a smooth install/update of gometalinter and its
-# linters:
+# The following should perform a smooth install/update of gometalinter and the
+# linter commands it vendors:
 GML="github.com/alecthomas/gometalinter"
-rm -rf "$GOPATH/src/$GML" # Possibly contains work the user cares about...?
+rm -rf "$GOPATH/src/$GML" # Possibly contains work the user cares about...? TODO(DH): Pass -I flag to rm?
 upstall "$GML"
 gometalinter --install --update
