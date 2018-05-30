362d8b967170e002c4f83423deb0797ecd4e9050
Duncan Holm <duncan@holm.scot>
2018-05-30 09:25:03 +0100

    Retire use of gometalinter:
    It's fundamentally too slow. This seems like the future: https://github.com/golangci/golangci-lint

--- install_tools.sh
+++ install_tools.sh
@@ -15,14 +15,3 @@ upstall github.com/nsf/gocode
 upstall golang.org/x/tools/cmd/goimports
 upstall golang.org/x/tools/cmd/gorename
 upstall golang.org/x/tools/cmd/guru
-
-# gometalinter does something complex where it vendors the source code of all
-# the linter commands it knows about, and itself provides a runtime flag to
-# take resposibility for building and installing them.
-#
-# The following should perform a smooth install/update of gometalinter and the
-# linter commands it vendors:
-GML="github.com/alecthomas/gometalinter"
-rm -rf "$GOPATH/src/$GML" # Possibly contains work the user cares about...? TODO(DH): Pass -I flag to rm?
-upstall "$GML"
-gometalinter --install
