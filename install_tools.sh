d26a84f234d3dfb710a9f37249f30573e8b3d310
Duncan Holm <duncan@holm.scot>
2018-05-31 09:08:26 +0100

    Add TODO

--- install_tools.sh
+++ install_tools.sh
@@ -11,6 +11,9 @@ upstall() {
 }
 
 upstall github.com/godoctor/godoctor
+# TODO(DH): Use the fork instead?
+# https://github.com/nsf/gocode/commit/9d1e0378d35b0527c9aef0d17c0913fc38d88b81
+# https://github.com/mdempsky/gocode
 upstall github.com/nsf/gocode
 upstall golang.org/x/tools/cmd/goimports
 upstall golang.org/x/tools/cmd/gorename
