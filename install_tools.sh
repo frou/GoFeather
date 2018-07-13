738c808b4033bae100b66a5648fad0a16eb0211c
Duncan Holm <duncan@holm.scot>
2018-07-13 14:15:08 +0100

    Comment

--- install_tools.sh
+++ install_tools.sh
@@ -14,6 +14,8 @@ upstall github.com/godoctor/godoctor
 # TODO(DH): Use the fork instead?
 # https://github.com/nsf/gocode/commit/9d1e0378d35b0527c9aef0d17c0913fc38d88b81
 # https://github.com/mdempsky/gocode
+# "I removed all of the config options" https://github.com/mdempsky/gocode/issues/13#issuecomment-220745687
+# But, there is: https://github.com/mdempsky/gocode/blob/9f6c12218c306acb79904362833755c25464b778/gocode.go#L18
 upstall github.com/nsf/gocode
 upstall golang.org/x/tools/cmd/goimports
 upstall golang.org/x/tools/cmd/gorename
