ae1a5e7ddf1ff26ec5162a742517841b020fce6f
Duncan Holm <duncan@holm.scot>
2019-02-17 12:38:29 +0000

    Populate the right-click context menu

--- plugin_contextmenu_dispatcher.py
+++ plugin_contextmenu_dispatcher.py
@@ -15,14 +15,28 @@ class gofeather_dispatch_contextmenu_click(sublime_plugin.TextCommand):
     def want_event(self):
         return True
 
-    def is_visible(self, event):
+    def is_visible(self, event, mode):
         return settings_indicate_go(self.view.settings())
         # pt = self.view.window_to_text((event["x"], event["y"]))
         # symbol, locations = symbol_at_point(self.view, pt)
         # return len(locations) > 0
 
-    def run(self, edit, event):
-        pass
+    def run(self, edit, event, mode):
+        # @todo #5 Discover the position in the text (line-column and/or byte offset) that the mouse context menu was activated at.
+        if mode == "lookup_docs":
+            pass
+        elif mode == "launch_docs":
+            pass
+        elif mode == "rename":
+            pass
+        elif mode == "simulate_rename":
+            pass
+        elif mode == "extract_function":
+            pass
+        elif mode == "query_interface":
+            pass
+        else:
+            sublime.message_dialog(mode)
         # pt = self.view.window_to_text((event["x"], event["y"]))
         # symbol, locations = symbol_at_point(self.view, pt)
         # navigate_to_symbol(self.view, symbol, locations)
