94e82b05a2d8daf0e126537bf8b86dda67ccab39
Duncan Holm <duncan@holm.scot>
2019-02-17 12:55:14 +0000

    Inspect the mouse event data

--- plugin_contextmenu_dispatcher.py
+++ plugin_contextmenu_dispatcher.py
@@ -8,7 +8,6 @@ from .util import *
 # TODU(DH): It should be the position of the mouse pointer, not the caret, that determines what is being renamed. See:
 # https://github.com/tomv564/LSP/commit/064883ce3955a5123bab54942bd83c0b827c5832
 # https://forum.sublimetext.com/t/get-point-under-mouse/29801/5
-# Could my command(s) just manually run a (view|window) "drag_select" command (with no args) as the first thing it does?
 # TODU(DH): See "TerminusOpenContextUrlCommand" in Terminus for a probably good implementation?
 
 class gofeather_dispatch_contextmenu_click(sublime_plugin.TextCommand):
@@ -17,12 +16,17 @@ class gofeather_dispatch_contextmenu_click(sublime_plugin.TextCommand):
 
     def is_visible(self, event, mode):
         return settings_indicate_go(self.view.settings())
-        # pt = self.view.window_to_text((event["x"], event["y"]))
-        # symbol, locations = symbol_at_point(self.view, pt)
-        # return len(locations) > 0
 
     def run(self, edit, event, mode):
-        # @todo #5 Discover the position in the text (line-column and/or byte offset) that the mouse context menu was activated at.
+        view = self.view
+
+        mouse_pos = (event["x"], event["y"])
+        byte_offset = view.window_to_text(mouse_pos)
+        line_col = view.rowcol(byte_offset)
+
+        # @todo #6 Move the text caret to the context menu activation position. Use either the drag_select command, or the symbol_at_point & navigate_to_symbol API?
+        sublime.message_dialog("%s at byte offset %s (line %d, column %d)" % (mode, byte_offset, line_col[0]+1, line_col[1]+1))
+
         if mode == "lookup_docs":
             pass
         elif mode == "launch_docs":
@@ -37,6 +41,3 @@ class gofeather_dispatch_contextmenu_click(sublime_plugin.TextCommand):
             pass
         else:
             sublime.message_dialog(mode)
-        # pt = self.view.window_to_text((event["x"], event["y"]))
-        # symbol, locations = symbol_at_point(self.view, pt)
-        # navigate_to_symbol(self.view, symbol, locations)
