9fd537cc3b134b7cc6fc5c511339edd09365c70a
Duncan Holm <duncan@holm.scot>
2019-03-07 11:26:29 +0000

    Add an interactive UI for interface query results

--- plugin_implements.py
+++ plugin_implements.py
@@ -3,6 +3,11 @@ import sublime_plugin
 
 from .util import *
 
+from .plugin_show_docs import determine_wd_for_cmd
+from .plugin_show_docs import submit_panel as show_documentation
+
+divider_indicator = "\t"
+
 class query_current_identifier_implements_or_implemented_by(sublime_plugin.TextCommand):
     def run(self, args):
         view = self.view
@@ -23,13 +28,73 @@ class query_current_identifier_implements_or_implemented_by(sublime_plugin.TextC
         sel0 = view.sel()[0]
 
         byte_offset = sel0.begin()
-        # TODU(DH): Pass the -json flag to get structured output, and then
-        # construct a nice readable presentation to present in the output
-        # panel. Can PATH:LINE patterns in a generic output panel be made
-        # clickable?
         guru_cmd = [
-            'guru', 'implements', "%s:#%d" %(file_path, byte_offset)
+            'guru', '--json', 'implements', "%s:#%d" %(file_path, byte_offset)
         ]
         guru_cmd_output = run_tool(guru_cmd)
-        if guru_cmd_output:
-            show_gofeather_output_panel(window, guru_cmd_output)
+        # print(guru_cmd_output)
+        if not guru_cmd_output:
+            return
+        decoded_json = sublime.decode_value(guru_cmd_output)
+
+        implements_something = "from" in decoded_json
+        implemented_by_something = "to" in decoded_json
+
+        if not implements_something and not implemented_by_something:
+            sublime.error_message("Doesn't implement any known interfaces.\n\nIsn't implemented by any known types.")
+            return
+
+        dividers_needed = implemented_by_something
+
+        results = []
+        if implements_something:
+            if dividers_needed:
+                results.append(self.make_divider("IMPLEMENTS"))
+            for desc in decoded_json["from"]:
+                results.append(desc["name"])
+
+        if implemented_by_something:
+            if dividers_needed:
+                results.append(self.make_divider("IMPLEMENTED BY"))
+            for desc in decoded_json["to"]:
+                results.append(desc["name"])
+
+        selected_idx = 0
+        if dividers_needed:
+            selected_idx = 1
+
+        # TODO(DH): Rather than a quick_panel, Use a ListInputHandler to allow a choice of what to do for each result:
+        # ResultA --> Show documentation
+        #         \-> Go to source code
+        # ResultB --> Show documentation
+        #         \-> Go to source code
+        # ...
+
+        window.show_quick_panel(
+            results,
+            lambda idx: self.handle_quick_panel_selection(results, idx),
+            sublime.KEEP_OPEN_ON_FOCUS_LOST,
+            selected_idx
+        )
+
+    def handle_quick_panel_selection(self, candidates, idx):
+        if idx < 0:
+            return
+
+        selection = candidates[idx]
+        if divider_indicator in selection:
+            return
+        # The leading * (denoting a pointer type) needs to be stripped to get gogetdoc(1) to work.
+        selection = selection.lstrip("*")
+
+        cmd_wd, cmd_wd_is_go_pkg = determine_wd_for_cmd(self.view)
+
+        show_documentation(
+            self.view.window(),
+            cmd_wd,
+            cmd_wd_is_go_pkg,
+            selection,
+            False)
+
+    def make_divider(self, text):
+        return "%s %s" % (divider_indicator, text)
